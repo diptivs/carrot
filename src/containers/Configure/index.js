2fcd45fb6739a15c6e17eb966e7a35fa4a4a6e16
Navjot Bola <navjobol@amazon.com>
2018-11-29 19:35:03 -0800

    init lex display

--- src/containers/Configure/index.js
+++ src/containers/Configure/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import LexChat from "react-lex";
 import "./configure.css";
 
 export default class Configure extends Component {
 	render() {
 		return (
 			<div className="configure-container">
-				Configuration here
+				<LexChat botName="OrderFlowers"
+                	IdentityPoolId="us-east-1:7292b8c0-56f1-4441-b2a6-xxxxxxxxxxxx"
+                	placeholder="Start by telling us when your day starts"
+                	backgroundColor="#FFFFFF"
+					height="430px"
+                	region="us-east-1"
+                	headerText="Configure here" />
 			</div>
 		);
 	}
