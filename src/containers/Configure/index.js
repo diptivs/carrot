03ec11da404849da58c5ff8dafd32c97cb75e76e
Parvizsho Aminov <parvizsho@gmail.com>
2018-12-07 04:15:12 -0800

    fixing bot preferences update

--- src/containers/Configure/index.js
+++ src/containers/Configure/index.js
@@ -14,12 +14,11 @@ const myTheme = {
 
 export default class Configure extends Component {
 
-	async handleComplete(err, config) {
+	handleComplete(err, config) {
 		if (err) {
 		  alert('Configuration setup failed')
 		  return;
 		}
-		console.log(config);
         var preference = { pomodoroSize: config.slots.pomodorosize,
                            shortBreakSize: config.slots.shortbreaksize,
                            longBreakSize: config.slots.longbreaksize,
@@ -31,22 +30,23 @@ export default class Configure extends Component {
         preference.workSchedule.end = this.convertTimeString(config.slots.workendtime);
         preference.workSchedule.lunch.start = this.convertTimeString(config.slots.lunchstarttime);
         preference.workSchedule.lunch.end = this.convertTimeString(config.slots.lunchendtime);
-        console.log(preference);
 
-        var existingPreference = await this.getPreferences();
-        console.log(existingPreference);
-        await this.setPreferences(preference, existingPreference);
+        this.setPreferences(preference).then(() => {
+            console.log("done with bot");
+        });
 
 		return 'Configuration is complete! Start adding some tasks';
 	}
 
-    setPreferences = (preference, existing) => {
-        if (existing) {
-            return API.put('api', `/api/preference/${existing.preferenceId}`,
-                           { body: preference });
-        } else {
-            return API.post('api', '/api/preference', { body: preference });
-        }
+    setPreferences = (preference) => {
+        return this.getPreferences().then( (existing) => {
+            if (existing.Count) {
+                return API.put('api', `/api/preference/${existing.Items[0].preferenceId}`,
+                            { body: preference });
+            } else {
+                return API.post('api', '/api/preference', { body: preference });
+            }
+        });
     }
 
     convertTimeString = (timestr) => {
@@ -59,6 +59,8 @@ export default class Configure extends Component {
     }
 
 	render() {
+        const existingPreference = this.getPreferences();
+        console.log("Existing", existingPreference);
 		return (
 			<div className="configure-container">
 				<ChatBot
