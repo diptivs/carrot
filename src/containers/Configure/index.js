add43111e33dbda379c6eb00c1185fbbfc86553a
Parvizsho Aminov <parvizsho@gmail.com>
2018-12-07 02:11:57 -0800

    lex changes

--- src/containers/Configure/index.js
+++ src/containers/Configure/index.js
@@ -1,3 +1,4 @@
+import { API } from "aws-amplify";
 import React, { Component } from "react";
 import "./configure.css";
 import config from "../../config";
@@ -13,15 +14,49 @@ const myTheme = {
 
 export default class Configure extends Component {
 
-	handleComplete(err, confirmation) {
+	async handleComplete(err, config) {
 		if (err) {
 		  alert('Configuration setup failed')
 		  return;
 		}
-	
-		alert('Success: ' + JSON.stringify(confirmation, null, 2));
+		console.log(config);
+        var preference = { pomodoroSize: config.slots.pomodorosize,
+                           shortBreakSize: config.slots.shortbreaksize,
+                           longBreakSize: config.slots.longbreaksize,
+                           workSchedule: { start: {},
+                                           lunch: { start: {}, end: {} },
+                                           end: {} } };
+
+        preference.workSchedule.start = this.convertTimeString(config.slots.workstarttime);
+        preference.workSchedule.end = this.convertTimeString(config.slots.workendtime);
+        preference.workSchedule.lunch.start = this.convertTimeString(config.slots.lunchstarttime);
+        preference.workSchedule.lunch.end = this.convertTimeString(config.slots.lunchendtime);
+        console.log(preference);
+
+        var existingPreference = await this.getPreferences();
+        console.log(existingPreference);
+        await this.setPreferences(preference, existingPreference);
+
 		return 'Configuration is complete! Start adding some tasks';
-	}	
+	}
+
+    setPreferences = (preference, existing) => {
+        if (existing) {
+            return API.put('api', `/api/preference/${existing.preferenceId}`,
+                           { body: preference });
+        } else {
+            return API.post('api', '/api/preference', { body: preference });
+        }
+    }
+
+    convertTimeString = (timestr) => {
+        var splitStr = timestr.split(':');
+        return { h: parseInt(splitStr[0]), m: parseInt(splitStr[1]) };
+    }
+
+    getPreferences = () => {
+        return API.get('api', '/api/preference');
+    }
 
 	render() {
 		return (
@@ -30,7 +65,7 @@ export default class Configure extends Component {
 					title="My Bot"
 					theme={myTheme}
 					botName="PomaFocus"
-					welcomeMessage="Welcome, start by telling me what time your day usually starts (i.e HH:00 AM)"
+					welcomeMessage="Welcome! My name is Poma, I will be helping you configure your accout. I will ask you several questions and setup your account preferences based on your answers. Shall we begin?"
 					onComplete={this.handleComplete.bind(this)}
 					clearOnComplete={true}
 				/>
