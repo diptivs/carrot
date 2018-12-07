5e13ee131c8a89e53c1ef388acf421f14f9db46b
Navjot Bola <navjotb1@gmail.com>
2018-12-06 21:28:15 -0800

    change state on google calandar

--- src/constants.js
+++ src/constants.js
@@ -8,8 +8,10 @@ const ADD_PROJECT_MODAL = {
     ADD_TITLE: "Add a Project"
 }
 const NONE_VALUE = "None"
+const LUNCH_TYPE = "lunch"
 module.exports = {
     TASK_STATUS,
     ADD_PROJECT_MODAL,
-    NONE_VALUE
+    NONE_VALUE,
+    LUNCH_TYPE
 }
